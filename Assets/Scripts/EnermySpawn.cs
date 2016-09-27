25a8262c2c9c11ba33a891a52d4b4ec5876ea903
Christina Lui <chrisyzlui@gmail.com>
2016-09-27 23:12:06 +0800

    First stable version checked in

---
+++ Assets/Scripts/EnermySpawn.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnermySpawn : MonoBehaviour {
+	public GameObject enermyPrefab;
+	GameObject enermyInstance;
+	bool startSpawn = false;
+
+	int i;
+	public int maxNumOfEnermy = 6;
+	float timer = 0;
+	float spawnTimeBetween = 3f;
+	float timerRecorder;
+	float leaveDelayTime = 15f;
+
+	// Use this for initialization
+	void Update () {
+		if (startSpawn) {
+			timer += Time.deltaTime;
+			if (timer >= spawnTimeBetween && i < maxNumOfEnermy) {
+				SpawnEnermy ();
+				timerRecorder = timer;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D obj){
+		if (obj.name == "Ship") {//need to unify name
+			Debug.Log ("enter trigger");
+			startSpawn = true;
+		} else {
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D obj){
+		if (obj.name == "Ship") {//need to unify name
+			Debug.Log ("leave trigger");
+			startSpawn = false;
+			//generate enermy at least once
+			SpawnEnermy ();
+		} else {
+		}
+	}
+
+	void SpawnEnermy(){
+		enermyInstance = (GameObject)Instantiate (enermyPrefab, transform.position, Quaternion.identity);
+		Debug.Log ("enermy borned");
+		timer = 0;
+		i+=1;
+	}
+
+}
---
+++ Assets/Scripts/PrestigeScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PrestigeScript : MonoBehaviour {
+
+    [SerializeField]
+    private Text valueText;
+
+    private int currValue = 0;
+    private int prevValue = 0;
+
+    public int Value
+    {
+        set
+        {
+            currValue = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(prevValue < currValue)
+        {
+            prevValue++;
+        }
+        else if (prevValue > currValue)
+        {
+            prevValue--;
+        }
+
+        valueText.text = "Prestige: " + prevValue.ToString();
+
+	}
+}
