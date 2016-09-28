cc6b7d22801c115bbb9428da0bb0e677fc0ac0ab
jwswensi <jwswensi@hotmail.com>
2016-09-28 15:47:01 +0800

    remove unnecessary codes

--- Assets/Scripts/EnemyDamageHandler.cs
+++ Assets/Scripts/EnemyDamageHandler.cs
@@ -9,14 +9,10 @@ public class EnemyDamageHandler : MonoBehaviour {
 
 	public int health = 1;
 
-	float invulnTimer = 0;
-
 	bool exploded = false;
 
-	int correctLayer;
-
 	void Start () {
-		correctLayer = gameObject.layer;
+		
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
@@ -42,22 +38,11 @@ public class EnemyDamageHandler : MonoBehaviour {
 			} else if (bullet.level == 5) {
 				health -= 10;
 			} 
-			invulnTimer = 1f;
-			gameObject.layer = 10;
+			//invulnTimer = 1f;
+			//gameObject.layer = 10;
 
 		}else if (other.name.StartsWith("SuperAttack")) {
 			health = 0;
-		}
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-		invulnTimer -= Time.deltaTime;
-
-		if(invulnTimer <= 0) {
-			gameObject.layer = correctLayer;
 		}
 
 		if (health <= 0 ) {
@@ -69,6 +54,12 @@ public class EnemyDamageHandler : MonoBehaviour {
 			Debug.Log ("health="+health);
 			Die();
 		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
 	}
 
 	void Die() {
