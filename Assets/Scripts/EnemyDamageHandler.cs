c8748c73239d6fb50dbe389cae42c3107f820752
Christina Lui <chrisyzlui@gmail.com>
2016-10-05 22:22:21 +0800

    merge with enemies version 2.0 manual

--- Assets/Scripts/EnemyDamageHandler.cs
+++ Assets/Scripts/EnemyDamageHandler.cs
@@ -9,27 +9,23 @@ public class EnemyDamageHandler : MonoBehaviour {
 
 	public int health = 1;
 
-	float invulnTimer = 0;
-
-	float deathDelay = 0;
-
 	bool exploded = false;
 
-	int correctLayer;
-
-	// Use this for initialization
 	void Start () {
-		correctLayer = gameObject.layer;
+		
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log ("other name is "+other.name);
+		//Debug.Log ("other name is "+other.name);
 		if (other.name == "Ship") {
 			if (clashShipDie) {
 				health = 0;
 			}
-		} else if (other.name.StartsWith ("CannonballPrefab")) {
-			Debug.Log("hitttttt");
+		} else if (other.name.StartsWith("Rock")) {
+			health = 0;
+			//Debug.Log("hit Rock health is "+health);
+		} else if (other.name.StartsWith("Cannonball")) {
+			//Debug.Log("hit Cannonball");
 			Shoot bullet = other.GetComponent<Shoot>();
 			if (bullet.level == 1) {
 				health -= 2;
@@ -42,55 +38,33 @@ public class EnemyDamageHandler : MonoBehaviour {
 			} else if (bullet.level == 5) {
 				health -= 10;
 			} 
+			//invulnTimer = 1f;
+			//gameObject.layer = 10;
 
-		}else if (isSameName(other.name,"Dynamite")) {
+		}else if (other.name.StartsWith("SuperAttack")) {
 			health = 0;
 		}
 
-		invulnTimer = 2f;
-		deathDelay = 1f;
-		gameObject.layer = 10;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-		invulnTimer -= Time.deltaTime;
-		deathDelay -= Time.deltaTime;
-
-		if(invulnTimer <= 0) {
-			gameObject.layer = correctLayer;
-		}
-
 		if (health <= 0 ) {
-			Debug.Log ("health = "+health);
 			if (!exploded && explode != null) {
 				Instantiate (explode, transform.position, transform.rotation);
 				exploded = true;
 			}
 
-			if(deathDelay <= 0) {
-				Debug.Log ("health="+health);
-				Debug.Log ("deathDelay="+deathDelay);
-				Die();
-			}
+			//Debug.Log ("health="+health);
+			Die();
 		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
 	}
 
 	void Die() {
-		Debug.Log ("I Die");
+		//Debug.Log ("I Die");
 		Destroy (gameObject);
 	}
 
-
-	bool isSameName(string name1, string name2) {
-
-		int bracketIndex1 = name1.IndexOf ("(");
-		int bracketIndex2 = name2.IndexOf ("(");
-
-		string n1 = name1.Substring (0, bracketIndex1 > 0 ? bracketIndex1 : name1.Length);
-		string n2 = name2.Substring (0, bracketIndex2 > 0 ? bracketIndex2 : name2.Length);
-
-		return n1.Equals (n2);
-	}
 }
