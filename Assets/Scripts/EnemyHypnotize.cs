417f42bdb29f5d1a0e777b69d88c66338f6b6226
jwswensi <jwswensi@hotmail.com>
2016-10-09 00:15:47 +0800

    1)hypnotize attack on hit the ship 2)update Jellyfish prefab

--- Assets/Scripts/EnemyHypnotize.cs
+++ Assets/Scripts/EnemyHypnotize.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class EnemyHypnotize : MonoBehaviour {
 
 	public GameObject bullet;
-	public float bulletOffset = 5f;
+	public float bulletOffset = 0f;
 	const float  delay = 0.5f;
 	float dieDelay = 0;
 	Transform ship;
 	bool stopAfterShock = true;
 	bool attacked = false;
+	bool hit = false;
 
 	ShipActions sa;	
 	EnemyDamageHandler edh;
@@ -20,25 +21,33 @@ public class EnemyHypnotize : MonoBehaviour {
 
 	}
 
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Ship") {
+			hit = true;
+			if (ship == null) {
+				ship = GameObject.Find ("Ship").transform;
+			}
+			if (sa == null) {
+				sa = ship.GetComponent<ShipActions>();
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
-
-		if (ship != null && sa == null) {
-			sa = ship.GetComponent<ShipActions>();
-		}
 
 		if (dieDelay > 0) {
 			dieDelay -= Time.deltaTime;
 		}
 
-		if (!attacked && withinAttackDistance () && sa != null) {
+		if (!attacked && hit && sa != null) {
 
 			sa.hypnotize = true;
 			attacked = true;
 			dieDelay = delay;
 			transform.gameObject.GetComponent<Renderer> ().material.color = Color.blue;
 			if (bullet != null) {
-				GameObject go = (GameObject)Instantiate (bullet, transform.position + getBulletOffset (), transform.rotation);
+				GameObject go = (GameObject)Instantiate (bullet, ship.position, transform.rotation);
 				go.transform.parent = ship;
 			}
 
@@ -53,56 +62,4 @@ public class EnemyHypnotize : MonoBehaviour {
 		}
 	}
 
-	Vector3 getBulletOffset () {
-
-		if (ship == null) {
-
-			GameObject target = GameObject.Find ("Ship");
-
-			if (target != null) {
-
-				ship = target.transform;
-			}
-		}
-
-		if (ship == null) {
-			return new Vector3 (0,0,0);
-		}
-
-		Vector3 dir = ship.position - transform.position;
-		Vector3 offset = dir.normalized * bulletOffset;
-
-		return offset;
-
-	}
-
-	bool withinAttackDistance () {
-
-		GameObject target = GameObject.Find ("Ship");
-		if (ship == null) {
-
-			if (target != null) {
-
-				ship = target.transform;
-			}
-		}
-
-		if (ship == null) {
-			return false;
-		}
-
-		float shipHeight = ship.gameObject.GetComponent<Renderer> ().bounds.size.y / 2;
-		float enemyHeight = transform.gameObject.GetComponent<Renderer> ().bounds.size.y / 2;
-
-		Vector3 dir = ship.position - transform.position;
-
-		if (dir.magnitude - shipHeight - enemyHeight < shipHeight * 0.5) {
-
-			return true;
-
-		}
-
-		return false;
-
-	}
 }
