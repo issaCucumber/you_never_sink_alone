511964187b8d9018f21464c8e73a48580108d527
MrGONG0028 <gong0028@e.ntu.edu.sg>
2016-10-09 18:43:13 +0800

    fix some bugs

--- Assets/Scripts/EnermyRandomMove.cs
+++ Assets/Scripts/EnermyRandomMove.cs
@@ -11,38 +11,52 @@ public class EnermyRandomMove : MonoBehaviour {
 	float zAngle;
 	float timer = 0;
 	// Update is called once per frame
+	bool stop = false;
+	bool stopWhenClashShip = false;
+	// Use this for initialization
+	void Start () {
+		stop = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (stopWhenClashShip && other.name == "Ship") {
+			stop = true;
+		}
+	}
 	void Update () {
-		if (ship == null) {
+		if (!stop) {
+			if (ship == null) {
 
-			GameObject target = GameObject.Find ("Ship");
+				GameObject target = GameObject.Find ("Ship");
 
-			if (target != null) {
-				//Debug.Log ("find ship");
-				ship = target.transform;
+				if (target != null) {
+					//Debug.Log ("find ship");
+					ship = target.transform;
+				}
 			}
+
+			if (ship == null) {
+				return;
+			}
+
+			randomY = Random.Range (0f, 70f);
+			randomX = Random.Range (0f, 70f);
+			randomRot = Random.Range (-2f, 2f);
+
+			//random is to change the face direction for now
+
+			//face direction
+			Vector3 dir = ship.position - transform.position;
+			zAngle = Mathf.Atan2 (dir.y + randomX, dir.x + randomY) * Mathf.Rad2Deg - 90;
+			Quaternion desiredRot = Quaternion.Euler (0, 0, zAngle);
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+
+
+			//movement
+			Vector3 pos = transform.position;
+			Vector3 velocity = new Vector3 (0, Random.Range (0, 1f) * maxSpeed * Time.deltaTime, 0);
+			pos += transform.rotation * velocity;
+			transform.position = pos;
 		}
-
-		if (ship == null) {
-			return;
-		}
-
-		randomY = Random.Range (0f, 70f);
-		randomX = Random.Range (0f, 70f);
-		randomRot = Random.Range (-2f,2f);
-
-		//random is to change the face direction for now
-
-		//face direction
-		Vector3 dir = ship.position - transform.position;
-		zAngle = Mathf.Atan2 (dir.y+randomX, dir.x+randomY) * Mathf.Rad2Deg - 90;
-		Quaternion desiredRot = Quaternion.Euler (0, 0, zAngle);
-		transform.rotation = Quaternion.RotateTowards (transform.rotation, desiredRot , rotSpeed * Time.deltaTime);
-
-
-		//movement
-		Vector3 pos = transform.position;
-		Vector3 velocity = new Vector3 (0, Random.Range(0,1f)*maxSpeed * Time.deltaTime, 0);
-		pos += transform.rotation * velocity;
-		transform.position = pos;
 	}
 }
